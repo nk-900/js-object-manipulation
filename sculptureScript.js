10369665c3640c27842522d62bca38f89ffa8314
nk-900 <nekpng@pm.me>
2025-10-22 22:37:02 -0700

    Completed assignment

--- sculptureScript.js
+++ sculptureScript.js
@@ -1,1 +1,29 @@
+/* Author: Ian Brown
+ * Date: 20251022
+ */
 
+const sculptureList = require('./data.js'); // import data.js
+
+const element = sculptureList[0] // <---- comment out this line in your solution!
+
+
+// this following snippet is just to show how to iterate an object's keys
+// comment this out in your solution!
+for (const key in element){
+    console.log(`${key}: ${element[key].length}`)
+}
+
+const sculptureListLengths = {};
+sculptureList.forEach((sculpture, index) => {
+    const keyLengths ={};
+
+    for (let key in sculpture) {
+        if (typeof sculpture[key] === 'string') {
+            keyLengths[key] = sculpture[key].length;
+        }
+    }
+
+    sculptureListLengths[index] = keyLengths;
+});
+
+console.log(sculptureListLengths);
